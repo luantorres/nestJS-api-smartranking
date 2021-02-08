c102ab6472fbf7237cde9208c82ace3f57599bb4
Luan Torres <luan-torres@hotmail.com>
2021-02-08 09:46:00 -0300

    global pipe and filter validation

--- src/common/pipes/parameters-validation.pipe.ts
+++ src/common/pipes/parameters-validation.pipe.ts
@@ -1,6 +1,6 @@
 import { ArgumentMetadata, BadRequestException, PipeTransform } from '@nestjs/common';
 
-export class PlayersParametersValidation implements PipeTransform {
+export class ParametersValidation implements PipeTransform {
     transform (value: any, metadata: ArgumentMetadata) {
         if (!value) {
             throw new BadRequestException(`O valor do parâmetro ${metadata.data} deve ser preenchido`);
